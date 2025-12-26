75a0360ca75303f63d4511d4e4a61bcc4785adb3
arthurjogos481-svg <arthurjogos481@gmail.com>
2025-12-26 09:22:06 -0300

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
     }
 
     const response = await fetch(
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=AIzaSyCGoBDkcTWYTV2jTN7NHYupCQQ0ZMWns6w",
+      "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=AIzaSyDlfJ-lHUvNweKZXaWwJ2CyIUEnFXIT3X4",
       {
         method: "POST",
         headers: {
